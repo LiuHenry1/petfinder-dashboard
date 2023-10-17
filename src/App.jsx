33243e1cfce53217152a15dd8631c44ebf4e8893
Henry Liu <hliu4176@gmail.com>
2023-10-17 18:34:50 -0500

    Feature: Add function to fetch a list of data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,61 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+const API_SECRET = import.meta.env.VITE_APP_API_SECRET;
+
 function App() {
+  let token = null;
+  const [animalList, setAnimalList] = useState(null);
+
+  useEffect(() => {
+    const getToken = async () => {
+      const resp = await fetch("https://api.petfinder.com/v2/oauth2/token", {
+        body: `grant_type=client_credentials&client_id=${API_KEY}&client_secret=${API_SECRET}`,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        method: "POST"
+      })
+      const data = await resp.json();
+      
+      console.log(data.access_token);
+      return data.access_token
+      }
+    
+      const getAnimals = async () => {
+        const resp = await fetch("https://api.petfinder.com/v2/animals", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+    
+        const data = await resp.json();
+
+        return data.animals;
+      }
+
+    const fetchData = async () => {
+      token = await getToken();
+      const animals = await getAnimals();
+
+      setAnimalList(animals);
+      console.log(animals);
+    }
+    fetchData();
+
+  }, []);
 
 
+
+  
   return (
     <>
-      
+      {animalList ? 
+        animalList.map(animal => {
+          return <li>{animal.name}, {animal.age}, {animal.species}</li>
+        }) : <div></div>
+      }
     </>
   )
 }
