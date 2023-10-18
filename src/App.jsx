cc53dd47d41bacaf5fc5e465e71302682e9b673d
Henry Liu <hliu4176@gmail.com>
2023-10-17 19:18:08 -0500

    Feature: Use Stat component to show summary of data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
-import './App.css'
-import List from './components/List';
+import { useEffect, useState } from "react";
+import "./App.css";
+import List from "./components/List";
+import Stat from "./components/Stat";
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 const API_SECRET = import.meta.env.VITE_APP_API_SECRET;
@@ -14,26 +15,26 @@ function App() {
       const resp = await fetch("https://api.petfinder.com/v2/oauth2/token", {
         body: `grant_type=client_credentials&client_id=${API_KEY}&client_secret=${API_SECRET}`,
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
+          "Content-Type": "application/x-www-form-urlencoded",
         },
-        method: "POST"
-      })
+        method: "POST",
+      });
       const data = await resp.json();
-    
-      return data.access_token
-      }
-    
-      const getAnimals = async () => {
-        const resp = await fetch("https://api.petfinder.com/v2/animals", {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        });
-    
-        const data = await resp.json();
 
-        return data.animals;
-      }
+      return data.access_token;
+    };
+
+    const getAnimals = async () => {
+      const resp = await fetch("https://api.petfinder.com/v2/animals", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await resp.json();
+
+      return data.animals;
+    };
 
     const fetchData = async () => {
       token = await getToken();
@@ -41,19 +42,25 @@ function App() {
 
       setAnimalList(animals);
       console.log(animals);
-    }
+    };
     fetchData();
-
   }, []);
 
-
-
-  
   return (
     <>
-      {animalList ? <List animalList={animalList} /> : <div></div>}
+      <h1>Pet Finder Dashboard</h1>
+      {animalList ? (
+        <div>
+          <Stat type="Number of results" value={animalList.length} />
+          <Stat type="Location" value="Set to none" />
+          <Stat type="Type" value="All" />
+          <List animalList={animalList} />
+        </div>
+      ) : (
+        <div></div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
