9167fe3c69a80c83f41aa6101aabc48e79e742ef
Henry Liu <hliu4176@gmail.com>
2023-10-24 20:20:12 -0500

    Feature: Use API call to fetch data
    Previously passed animalList array from App.jsx to AnimalDetail.jsx but
    animalList would be null because this component does not call the
    function to fetch data. In this current implementation, a function is
    passed as parameter that allows this component to fetch the needed data.

--- src/components/AnimalDetail.jsx
+++ src/components/AnimalDetail.jsx
@@ -1,43 +1,57 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-const AnimalDetail = ({ animalList }) => {
-  if (animalList == null) {
-    return;
-  }
-
+const AnimalDetail = ({ getAnimal }) => {
+  const [animal, setAnimal] = useState(null);
   const params = useParams();
+  useEffect(() => {
+    const fetchData = async () => {
+      const animalToDisplay = await getAnimal(params.id);
 
-  const animal = animalList[params.index];
+      setAnimal(animalToDisplay);
+    };
+
+    fetchData();
+  }, []);
+  console.log(animal);
 
   return (
-    <div className="detailed-view">
-      <div className="intro">
-        <h1>{animal.name}</h1>
-        <img width="600" height="600" src={animal.photos[0]["large"]} />
-      </div>
-      <div className="overview">
-        <div className="main">
-          <h4>Overview</h4>
-          <li>Species: {animal.type}</li>
-          <li>Breed: {animal.breeds.primary}</li>
-          <li>Gender: {animal.gender} </li>
-          <li>Age: {animal.age}</li>
-          <li>Description: {animal.description}</li>
-          <li>Contact: {animal.contact.email}</li>
+    <>
+      {animal ? (
+        <div className="detailed-view">
+          {animal.photos.length > 0 ? (
+            <img width="500" height="500" src={animal.photos[0].medium} />
+          ) : (
+            <div> </div>
+          )}
+          <div className="overview">
+            <div className="primary-info">
+              <h4>Overview</h4>
+              <li>Name: {animal.name}</li>
+              <li>Species: {animal.type}</li>
+              <li>Breed: {animal.breeds.primary}</li>
+              <li>Gender: {animal.gender} </li>
+              <li>Age: {animal.age}</li>
+              <li>Description: {animal.description}</li>
+              <li>Contact: {animal.contact.email}</li>
+            </div>
+            <div className="additional-info">
+              <h4>Additional Information</h4>
+              {Object.keys(animal.attributes).map((attribute) => {
+                return (
+                  <li>
+                    {attribute.replace("_", " ")}:{" "}
+                    {animal.attributes[attribute] ? "Y" : "N"}
+                  </li>
+                );
+              })}
+            </div>
+          </div>
         </div>
-        <div className="additional-info">
-          <h4>Additional Information</h4>
-          {Object.keys(animal.attributes).map((attribute) => {
-            return (
-              <li>
-                {attribute.replace("_", " ")}:{" "}
-                {animal.attributes[attribute] ? "Y" : "N"}
-              </li>
-            );
-          })}
-        </div>
-      </div>
-    </div>
+      ) : (
+        <div></div>
+      )}
+    </>
   );
 };
 
