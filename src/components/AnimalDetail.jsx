d76ed435e884380fef2ce372a1d84ffed58b5bbd
Henry Liu <hliu4176@gmail.com>
2023-10-24 19:27:20 -0500

    Feature: Add more information to display and change <p> to <li>

--- src/components/AnimalDetail.jsx
+++ src/components/AnimalDetail.jsx
@@ -8,19 +8,34 @@ const AnimalDetail = ({ animalList }) => {
   const params = useParams();
 
   const animal = animalList[params.index];
-  console.log(animal);
 
   return (
     <div className="detailed-view">
-      <h1>{animal.name}</h1>
-      <img src={animal.photos[0]["large"]} />
-      <div className="animal-details">
-        <p>Species: {animal.type}</p>
-        <p>Breed: {animal.breeds.primary}</p>
-        <p>Gender: {animal.gender} </p>
-        <p>Age: {animal.age}</p>
-        <p>Description: {animal.description}</p>
-        <p>Contact: {animal.contact.email}</p>
+      <div className="intro">
+        <h1>{animal.name}</h1>
+        <img width="600" height="600" src={animal.photos[0]["large"]} />
+      </div>
+      <div className="overview">
+        <div className="main">
+          <h4>Overview</h4>
+          <li>Species: {animal.type}</li>
+          <li>Breed: {animal.breeds.primary}</li>
+          <li>Gender: {animal.gender} </li>
+          <li>Age: {animal.age}</li>
+          <li>Description: {animal.description}</li>
+          <li>Contact: {animal.contact.email}</li>
+        </div>
+        <div className="additional-info">
+          <h4>Additional Information</h4>
+          {Object.keys(animal.attributes).map((attribute) => {
+            return (
+              <li>
+                {attribute.replace("_", " ")}:{" "}
+                {animal.attributes[attribute] ? "Y" : "N"}
+              </li>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
